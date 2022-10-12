1bf6266ea0d5257983267ef97e55c5e9a88dc2de
Risto Tõldsep <r.toldsep@gmail.com>
2022-10-13 02:58:52 +0300

    Got top charts working and added view for song lyrics

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -43,14 +43,14 @@ const SongCard = ({ song, i, activeSong, isPlaying, data }) => {
 
       <div className="mt-4 flex flex-col">
         <p className="font-semibold text-lg text-white truncate">
-          <Link to={`/songs/{song?.key}`}>{song.title}</Link>
+          <Link to={`/songs/${song?.key}`}>{song.title}</Link>
         </p>
-        <p className="text-sm truncate text-gray-300 mt-1">
+        <p className="text-sm truncate text-gray-300 mt-1 hover:text-white">
           <Link
             to={
               song.artists
                 ? `/artists/${song?.artists[0]?.adamid}`
-                : "/top-artists"
+                : '/top-artists'
             }
           >
             {song.subtitle}
